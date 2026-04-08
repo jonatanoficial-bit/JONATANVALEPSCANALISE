2c366edd250e37fc345a0b6e06b4c9e413acc4d9
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-04-08 12:58:47 -0300

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -6,7 +6,7 @@ window.SITE_CONFIG = {
   email: 'jonatanoficial@gmail.com',
   instagram: 'https://instagram.com/jonatan_vale_Oficial',
   facebook: 'https://facebook.com/jonatan.vale.7',
-  appointmentLink: 'COLE_AQUI_SEU_LINK_DO_GOOGLE_APPOINTMENT_SCHEDULE',
+  appointmentLink: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3dWk0D-_eI3MZE69_8XNmMhbjbGkPn1jpBZPsvQHxUIHCQKQ_jFImKyL1YC2KeCpBB3o7w4B-k?gv=true',
   meetInfo: 'Após a confirmação pelo Google Agenda, o paciente recebe o acesso da consulta online.',
   fallbackBookingLink: 'https://wa.me/5511968272371?text=Olá%20Jonatan%2C%20quero%20agendar%20uma%20consulta%20e%20ainda%20não%20encontrei%20o%20link%20da%20agenda.'
 };
